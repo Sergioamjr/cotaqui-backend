fba5ebd2fa22e6b0e00dff06fa1c5d26ae5ef73b
Sergioamjr <sergioamjr91@gmail.com>
2019-02-10 13:22:22 -0200

    Fix delete

--- src/schemas/vendas/index.js
+++ src/schemas/vendas/index.js
@@ -1,26 +1,26 @@
-const Vendas = require("./vendasSchema.js");
-const _get = require("lodash/get");
-const Controller = require("./../Controller.js/Controller.js");
+const Vendas = require('./vendasSchema.js');
+const _get = require('lodash/get');
+const Controller = require('./../Controller.js/Controller.js');
 
 const createVenda = (req, res) => {
-  const venda = _get(req, "body.venda");
+  const venda = _get(req, 'body.venda');
   const novaVenda = new Vendas({ ...venda });
   novaVenda.save((error, venda) => {
     if (error) {
-      res.json({ errorMessage: "Erro ao criar venda", error });
+      res.json({ errorMessage: 'Erro ao criar venda', error });
     } else {
-      res.json({ response: "Venda criada com sucesso", venda });
+      res.json({ response: 'Venda criada com sucesso', venda });
     }
   });
 };
 
 const deleteVenda = (req, res) => {
-  const _id = _get(req, "body._id");
-  Controller.removeItem(res, Vendas, _id, "VENDA");
+  const _id = _get(req, 'query._id');
+  Controller.removeItem(res, Vendas, _id, 'VENDA');
 };
 
 const queryVenda = (req, res) => {
-  Controller.findItem(res, Vendas, {}, "VENDA");
+  Controller.findItem(res, Vendas, {}, 'VENDA');
 };
 
 module.exports = {
