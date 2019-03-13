d0cf4497e030022c7cbe61d0be2efac96493480e
Sergioamjr <sergioamjr91@gmail.com>
2019-03-13 17:00:07 -0300

    update port and database url

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,17 @@
 var mongoose = require('mongoose');
-const url = 'mongodb://localhost/cotaqui';
+const localDB = 'mongodb://localhost/cotaqui';
+
+const url = process.env.MONGODB_URI || localDB;
 
 module.exports = mongoose.connect(url, { useNewUrlParser: true });
+
+// mongodb://usuario:senha@tatooine.mongodb.umbler.com:41091/nomedobanco
+// mongodb://cotaquionline-us:1q2w3e4e@kamino.mongodb.umbler.com:45144/cotaquionline-bd
+
+// mongo_cotaquionline-bd:27017
+
+// kamino.mongodb.umbler.com:45144
+
+// cotaquionline-us
+
+// 1q2w3e4e
